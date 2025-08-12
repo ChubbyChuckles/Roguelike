c41bc643618c0a327e8f8bb063e9ae8d3191c9f6
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-08-13 01:11:36 +0200

    added password check to hard reset script

--- hard_reset_repo.ps1
+++ hard_reset_repo.ps1
@@ -1,3 +1,11 @@
+## Password prompt before reset
+$password = Read-Host -AsSecureString 'Enter password to reset the repo'
+$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+$plain = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+if ($plain -ne 'egon') {
+    Write-Host 'Incorrect password. Aborting.' -ForegroundColor Red
+    exit 1
+}
 param(
     [Parameter(Mandatory=$true)]
     [string]$CommitId
