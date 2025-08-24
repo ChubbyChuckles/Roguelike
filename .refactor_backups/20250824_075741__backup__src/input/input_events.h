a578a57e4c412a23164f967d682a28c8f2db13be
ChubbyChuckles <christian.rickert.1989@gmail.com>
2025-08-24 08:03:34 +0200

    removed restructure backups

--- .refactor_backups/20250824_075741__backup__src/input/input_events.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROGUE_INPUT_EVENTS_H
-#define ROGUE_INPUT_EVENTS_H
-#include "app/app_state.h"
-void rogue_process_events(void);
-/* Process any skill activations queued during event polling (run after movement). */
-void rogue_process_pending_skill_activations(void);
-#endif /* ROGUE_INPUT_EVENTS_H */
